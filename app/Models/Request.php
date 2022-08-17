54aeb9e8e23eda75471a623cf4d73ca09e111f53
Ibrokhim001 <sayliyevibrohi001777000@gmail.comm>
2022-08-17 15:29:07 +0300

    banner, request, contact done

--- app/Models/Request.php
+++ app/Models/Request.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Request extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+            'full_name',
+            'phone',
+            'email',
+            'portfolio',
+            'cover_letter_uz',
+            'cover_letter_ru',
+            'cover_letter_en',
+    ];
 }
