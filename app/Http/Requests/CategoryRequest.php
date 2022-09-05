3afc3a359ced5cdc1b8bd42da116d533eb37552f
Ozodbek Juraev <ozodbekmcfly@gmail.com>
2022-09-05 17:46:06 +0500

    Image request

---
+++ app/Http/Requests/CategoryRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name_uz' => 'required|string|max:30',
+            'name_ru' => 'required|string|max:30',
+            'name_en' => 'required|string|max:30',
+            'image' => 'required|mimes:jpeg,png,jpg,gif,svg',
+        ];
+    }
+}
