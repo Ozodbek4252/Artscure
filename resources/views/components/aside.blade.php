053a0ef42828109b5866814bb05d9a056fa1aa91
YuldashovAhror <ahroryuldashov123@gmail.com>
2023-01-08 00:31:18 +0500

    aside add tools after file done

--- resources/views/components/aside.blade.php
+++ resources/views/components/aside.blade.php
@@ -84,6 +84,103 @@
             </a>
         </li>
 
+
+
+        <li class="menu-item
+        @if(\Request::route()->getName() == 'tools.index')
+        active
+        @endif
+        ">
+            <a href="{{ Route('tools.index') }}" class="menu-link">
+                <i class='bx bx-group'></i>
+                <div data-i18n="Artists">Artists</div>
+            </a>
+        </li>
+
+        <li class="menu-item
+        @if(\Request::route()->getName() == 'tools.index')
+        active
+        @endif
+        ">
+            <a href="{{ Route('tools.index') }}" class="menu-link">
+                <i class='bx bx-layer' ></i>
+                <div data-i18n="Products">Products</div>
+            </a>
+        </li>
+
+        <li class="menu-item
+        @if(\Request::route()->getName() == 'tools.index')
+        active
+        @endif
+        ">
+            <a href="{{ Route('tools.index') }}" class="menu-link">
+                <i class='bx bx-spreadsheet' ></i>
+                <div data-i18n="Orders">Orders</div>
+            </a>
+        </li>
+
+        <li class="menu-item
+        @if(\Request::route()->getName() == 'tools.index')
+        active
+        @endif
+        ">
+            <a href="{{ Route('tools.index') }}" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-calendar"></i>
+                <div data-i18n="Helps">Helps</div>
+            </a>
+        </li>
+
+        <li class="menu-item
+        @if(\Request::route()->getName() == 'tools.index')
+        active
+        @endif
+        ">
+            <a href="{{ Route('tools.index') }}" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-calendar"></i>
+                <div data-i18n="Requests">Requests</div>
+            </a>
+        </li>
+
+        <li class="menu-item
+        @if(\Request::route()->getName() == 'tools.index')
+        active
+        @endif
+        ">
+            <a href="{{ Route('tools.index') }}" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-calendar"></i>
+                <div data-i18n="Contacts">Contacts</div>
+            </a>
+        </li>
+
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bx-food-menu"></i>
+                <div data-i18n="News">News</div>
+            </a>
+            <ul class="menu-sub">
+                <li class="menu-item">
+                    <a href="app-invoice-list.html" class="menu-link">
+                        <div data-i18n="List">List</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="app-invoice-add.html" class="menu-link">
+                        <div data-i18n="Add">Add</div>
+                    </a>
+                </li>
+            </ul>
+        </li>
+
+        <li class="menu-item
+        @if(\Request::route()->getName() == 'tools.index')
+        active
+        @endif
+        ">
+            <a href="{{ Route('tools.index') }}" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-calendar"></i>
+                <div data-i18n="Banners">Banners</div>
+            </a>
+        </li>
         <!-- Layouts -->
         <li class="menu-item">
             <a href="javascript:void(0);" class="menu-link menu-toggle">
